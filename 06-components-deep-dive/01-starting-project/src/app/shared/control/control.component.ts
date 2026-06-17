44a3b674f76b2b264fda03b46072cdf63d3f9c21
Emanuel Vargas <reponzo01@gmail.com>
2026-06-17 12:48:38 -0400

    134. ViewChild vs ContentChild

--- 06-components-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
+++ 06-components-deep-dive/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   Component,
+  contentChild,
+  ContentChild,
   ElementRef,
   inject,
   input,
@@ -25,6 +27,14 @@ export class ControlComponent {
   // }
   label = input.required<string>();
   private el = inject(ElementRef);
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
 
-  onClick() {}
+  onClick() {
+    console.log('Clicked');
+    console.log(this.el);
+    console.log(this.control());
+  }
 }
