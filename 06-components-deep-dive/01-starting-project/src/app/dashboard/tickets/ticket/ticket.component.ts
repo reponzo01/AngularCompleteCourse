e5ee7bf8910764a53fdd78ac31b22aa3e5ad93a7
Emanuel Vargas <reponzo01@gmail.com>
2026-06-17 21:41:29 -0400

    146. Configuring Component Inputs & Outputs

--- 06-components-deep-dive/01-starting-project/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-components-deep-dive/01-starting-project/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -9,7 +9,11 @@ import { Ticket } from '../ticket.model';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  // @Input({})...
+  // You can set up a transformer function, or an alias which will change the name of the input listened to on the component but will keep the variable name. Same with the output
+  // ticket = input.required<Ticket>({alias: 'data'})
   data = input.required<Ticket>();
+  // @Output('closeTicket')...
   close = output();
   detailsVisible = signal(false);
 
