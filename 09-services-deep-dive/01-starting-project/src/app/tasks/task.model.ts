2b554a42c9f6cddd0df61ee3e17963ebd527f02e
Emanuel Vargas <reponzo01@gmail.com>
2026-06-22 18:23:41 -0400

    190. Injecting Other Values (NOT Services)

--- 09-services-deep-dive/01-starting-project/src/app/tasks/task.model.ts
+++ 09-services-deep-dive/01-starting-project/src/app/tasks/task.model.ts
@@ -1,26 +1,39 @@
+import { InjectionToken, Provider } from '@angular/core';
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
-export const TaskStatusOption: {
+type TaskStatusOptions = {
   value: 'open' | 'in-progress' | 'done';
-  taskStatus: TaskStatus,
+  taskStatus: TaskStatus;
   text: string;
-}[] = [
+}[];
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>(
+  'task-status-options',
+);
+
+export const TaskStatusOptions: TaskStatusOptions = [
   {
     value: 'open',
     taskStatus: 'OPEN',
-    text: 'Open'
+    text: 'Open',
   },
   {
     value: 'in-progress',
     taskStatus: 'IN_PROGRESS',
-    text: 'In-Progress'
+    text: 'In-Progress',
   },
   {
     value: 'done',
     taskStatus: 'DONE',
-    text: 'Completed'
+    text: 'Completed',
   },
-]
+];
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatusOptions,
+};
 
 export interface Task {
   id: string;
