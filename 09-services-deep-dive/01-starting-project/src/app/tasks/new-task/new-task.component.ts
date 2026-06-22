8624294591a4872fb1064dea4196ddc75cddd517
Emanuel Vargas <reponzo01@gmail.com>
2026-06-21 22:32:58 -0400

    179. Using Angular's Dependency Injection Mechanism

--- 09-services-deep-dive/01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 09-services-deep-dive/01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -11,11 +11,8 @@ import { TasksService } from '../tasks.service';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
-  private tasksService: TasksService;
 
-  constructor() {
-    this.tasksService = new TasksService();
-  }
+  constructor(private tasksService: TasksService) {}
 
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({ title, description });
