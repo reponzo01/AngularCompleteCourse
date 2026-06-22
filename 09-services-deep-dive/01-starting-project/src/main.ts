9443a52388c9c59415762be55c1ed1ca8c9e6d79
Emanuel Vargas <reponzo01@gmail.com>
2026-06-22 17:32:03 -0400

    183. There Are Multiple Ways Of Providing a Service

--- 09-services-deep-dive/01-starting-project/src/main.ts
+++ 09-services-deep-dive/01-starting-project/src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksService } from './app/tasks/tasks.service';
 
+// This makes it so that you don't need the @Injectable decorator, but it is not recommended since it can't optimize the code bundle that's shipped.
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService]
+// }).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
