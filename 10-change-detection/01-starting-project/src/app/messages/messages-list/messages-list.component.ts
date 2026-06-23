66d7967ebd3bd59177eeb49eb0124fe335e9d1d8
Emanuel Vargas <reponzo01@gmail.com>
2026-06-22 21:33:09 -0400

    204. Triggering Change Detection Manually & Using RxJS Subjects

--- 10-change-detection/01-starting-project/src/app/messages/messages-list/messages-list.component.ts
+++ 10-change-detection/01-starting-project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -6,12 +13,25 @@ import { MessagesService } from '../messages.service';
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messagesService = inject(MessagesService);
-  get messages() {
-    return this.messagesService.allMessages;
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
+
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe(
+      (messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      },
+    );
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   get debugOutput() {
