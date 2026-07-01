c60a17426c6260a0bcb4d7c95703afa5c23bda9c
Emanuel Vargas <reponzo01@gmail.com>
2026-07-01 12:08:42 -0400

    250. Updating Form Values Programmatically

--- 13-forms/01-starting-project/src/app/auth/login/login.component.ts
+++ 13-forms/01-starting-project/src/app/auth/login/login.component.ts
@@ -21,13 +21,24 @@ export class LoginComponent {
 
   constructor() {
     afterNextRender(() => {
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) =>
-          window.localStorage.setItem(
-            'saved-login-form',
-            JSON.stringify({ email: value.email }),
-          ),
-      });
+      const savedForm = window.localStorage.getItem('saved-login-form');
+
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({ email: value.email }),
+            ),
+        });
 
       this.destroyRef.onDestroy(() => subscription?.unsubscribe());
     });
